949eb7f49ed6dca6108d226650962425e59bd9d4
israel <israel@cloudware.mx>
2021-01-21 12:57:03 -0500

    se agrega version en español y flags de idiomas

--- js/index.js
+++ js/index.js
@@ -51,7 +51,11 @@ $(function () {
   });
   $("#addPerson").on("click", function (e) {
     e.preventDefault();
-    addPerson();
+    if (navigator.language == "en-MX") {
+      addPersonEs();
+    } else {
+      addPerson();
+    }
   });
   $(".close").on("click", function (e) {
     e.preventDefault();
@@ -233,6 +237,189 @@ $(function () {
     window.crypto.getRandomValues(arr);
     return Array.from(arr, dec2hex).join("");
   }
+  function addPersonEs() {
+    id = id + 1;
+    var template = `<div id="guest-${id}" class="formulario_formulario" data-id="${id}">
+    <div class="row guest">
+      <div class="dataForm"><strong>Huesped ${id + 1}</strong></div>
+      <div class="dataForm" style="align-items: flex-end">
+        <strong data-id="${id}" class="close">X</strong>
+      </div>
+    </div>
+    <div class="row">
+      <div class="dataForm">
+        <label for="">Nombre completo <small> (*como en su pasaporte)</small></label>
+        <input type="text" class="name" name="name" placeholder="" />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>
+      <div class="dataForm">
+        <label for="">Email</label>
+        <input
+          name="email"
+          class="email"
+          type="email"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>
+    </div>
+    <div class="row">
+      <div class="dataForm cumpleanos">
+        <label for="">Fecha de nacimiento</label>
+        <input
+          type="text"
+          name="birthdate"
+          class="caleran birthdate"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>
+      <div class="dataForm">
+        <label for="">No. pasaporte *</label>
+        <input
+          type="text"
+          name="passport"
+          class="passport"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>      
+    </div>
+    <div class="row">
+      <div class="dataForm">
+        <label for="">Tipo de prueba *</label>
+        <div class="options">
+          <div class="option">
+            <input
+              type="radio"
+              checked
+              value="Antigen"
+              placeholder=""
+              name="type-${id}"
+              id="Antigen-${id}"
+              class="type"
+            />
+            <label for="Antigen-${id}">Antígeno</label>
+          </div>
+          <div class="option">
+            <input
+              type="radio"
+              value="PCR"
+              id="PCR-${id}"
+              class="type"
+              placeholder=""
+              name="type-${id}"
+            />
+            <label for="PCR-${id}">PCR</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="dataForm">
+        <label for="">No. Villa *</label>
+        <input
+          type="text"
+          class="villa"
+          name="villa"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >No. Villa o No. Reservación requerido</small
+        >
+      </div>
+      <div
+        style="
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 0px 10px;
+          font-weight: bold;
+        "
+      >
+        OR
+      </div>
+      <div class="dataForm">
+        <label for="">No. Reservación *</label>
+        <input
+          type="text"
+          class="reservationNumber"
+          name="reservationNumber"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >No. Villa o No. Reservación requerido</small
+        >
+      </div>
+    </div>
+    <div class="row">
+      <div class="dataForm">
+        <label for="">Fecha salida *</label>
+        <input
+          type="text"
+          name="departure"
+          class="caleran departure"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>
+      <div class="dataForm">
+        <label for="">Hora vuelo *</label>
+        <input
+          type="text"
+          class="flight"
+          name="flight"
+          placeholder=""
+        />
+        <small class="error" style="display: none"
+          >Campo requerido</small
+        >
+      </div>
+    </div>
+  </div>`;
+    $(template).appendTo(".guests");
+    $(`#guest-${id} .caleran.birthdate`).caleran({
+      showHeader: false,
+      showFooter: false,
+      singleDate: true,
+      calendarCount: 1,
+      format: "DD-MM-YYYY",
+      autoCloseOnSelect: true,
+    });
+    $(`#guest-${id} .caleran.departure`).caleran({
+      minDate: new moment(),
+      showHeader: false,
+      showFooter: false,
+      singleDate: true,
+      calendarCount: 1,
+      format: "DD-MM-YYYY",
+      autoCloseOnSelect: true,
+    });
+    $(`#guest-${id} .close`).on("click", function (e) {
+      e.preventDefault();
+      // id="guest-0"
+      // addPerson();
+      var idToRevome = $(this).data("id");
+      $("#guest-" + idToRevome).remove();
+    });
+    $(`#guest-${id} .flight`).timepicker({
+      timeFormat: "h:mm p",
+      interval: 15,
+      dynamic: false,
+      dropdown: true,
+      scrollbar: true,
+    });
+  }
   function addPerson() {
     id = id + 1;
     var template = `<div id="guest-${id}" class="formulario_formulario" data-id="${id}">
@@ -277,7 +464,7 @@ $(function () {
         >
       </div>
       <div class="dataForm">
-        <label for="">Passport *</label>
+        <label for="">Passport Number *</label>
         <input
           type="text"
           name="passport"
